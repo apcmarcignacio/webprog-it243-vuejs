49a7645a70f8472e1a79681cfcdca15c27d37d53
Marc René Ignacio <mmignacio2@student.apc.edu.ph>
2026-01-22 03:56:40 +0800

    personal profile web test commit

---
+++ firstsfc/src/components/PersonalProfile.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <header> <h1>Personal Web Profile</h1> <header>
+        <main>
+            <section class="main-content">
+                <body>
+                    <h1 class="center">Personal Profile Webpage</h1>
+                <p>
+
+                About Me<br>
+                    Name: Marc Rene M. Ignacio
+  
+                Education<br>
+                    Senior Highschool Graduate
+
+                Course<br>
+                    Bachelor of Science in Information Technology
+
+                Hobbies/Interests<br>
+                    Baskletball, Movies, Cards
+
+                Goals in Life<br>
+                    To be financially free<br>
+
+                Picture Gallery<br>
+                    <img src="https://www.w3schools.com/html/img_girl.jpg" alt="Girl with a jacket" width="500" height="600">
+  
+  
+                </p>
+                </body>
+    <div>
+
+<script></script>
+<style></style>
